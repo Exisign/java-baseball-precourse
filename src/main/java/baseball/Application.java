fe8ea597ed774f82709aeaa3671d59af6123ff44
exisign <exisign777@gmail.com>
2022-04-18 01:33:14 +0900

    controller 시행 부 업로드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import baseball.message.Message;
+import baseball.model.BaseBallResult;
+import baseball.rule.BaseBall;
+
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBall baseBall = BaseBall.getInstacne();
+        boolean isFinished = false;
+        while (!isFinished) {
+            BaseBallResult baseBallResult = baseBall.getResult(Message.getNumber());
+            Message.showResult(baseBallResult);
+            isFinished = Message.checkGameOver(baseBallResult);
+            if(isFinished)
+                isFinished = !baseBall.isInit(Message.getEndNumber());
+        }
     }
 }
